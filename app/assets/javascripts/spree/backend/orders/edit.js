b411fbf8582bbd4d0294974e3e8661931c14a473
bvnbase <bvnbase@gmail.com>
2015-05-11 15:48:57 +0700

    + Add function edit delivery status for line item

--- app/assets/javascripts/spree/backend/orders/edit.js
+++ app/assets/javascripts/spree/backend/orders/edit.js
@@ -2,16 +2,99 @@ $(document).ready(function () {
   'use strict';
 
   $('[data-hook="add_product_name"]').find('.variant_autocomplete').variantAutocomplete();
+
 });
 
+jQuery(function($) {
+  // Make select beautiful
+  $('select.select3').select2({
+    allowClear: true,
+    dropdownAutoWidth: true
+  });
+});
 
-$(document).on("click", ".dow-block", function()
+$(document).on("click", ".expand-trigger", function()
 {
-  var objectContent = $(this).find(".content");
+  var objectContent = $(this).parent().parent().find(".content");
   var icon = $(this).find(".icon");
 
   $(icon).toggleClass("expand");
 
-  objectContent.slideToggle();
+  objectContent.stop().slideToggle();
 
 });
+
+$(document).on("click", ".dow-block .title .status.show", function()
+{ 
+  //console.log("edit");
+  toggleEdit(this);
+});
+
+$(document).on("click", ".dow-block .title .status.edit", function()
+{
+
+});
+
+//Save button
+$(document).on("click", ".dow-block .title .button.save", function()
+{
+  var orderId = $(".order-info").attr("data-order-id");
+  var status = $(this).parent().find("select.select3").val();
+  var dateDelivery = $(this).parent().parent().find(".date-delivery").text();
+
+  // console.log($(this).parent().find("select.select3"));
+  // console.log("STATUS: " + status);
+
+  UpdateLineItemStatus(orderId, status, dateDelivery, $(this));
+
+});
+
+$(document).on("click", ".dow-block .title .button.cancel", function()
+{
+  toggleEdit(this);
+});
+
+
+function toggleEdit(that)
+{
+  $(that).parent().find(".status.edit").toggle();
+
+  if($(that).parent().find(".status.edit").is(":visible"))
+  {
+    var status = $(that).parent().find(".status.show span").text().trim();
+    console.log("STATUS: " + status);
+    $(that).parent().find("select.select3").select2("val", status);
+  }
+
+  $(that).parent().find(".status.show").toggle();
+  $(that).parent().find(".button.save").toggle();
+  $(that).parent().find(".button.cancel").toggle();
+}
+
+//=====================
+function UpdateLineItemStatus(orderId, status, dateDelivery, that)
+{
+  var apiUrl = Spree.routes.orders_api = Spree.pathFor('api/orders') + "/" + orderId + "/line_items_status";
+
+  $.ajax({
+    type: "post",
+    url: apiUrl,
+    data:
+    {
+      line_item : {
+        status: status,
+        delivery_date: dateDelivery
+      },
+      token: Spree.api_key
+    },
+    success: function(r)
+    {
+      show_flash("success", "Update line item status successfully!");
+      toggleEdit(that);
+    },
+    error: function(e)
+    {
+      show_flash("error", "Error!");
+    }
+  });
+}
